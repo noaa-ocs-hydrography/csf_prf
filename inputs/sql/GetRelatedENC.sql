bd593b50ba67c3c2c8baa2576fcc93e089f48c34
stephen.patterson <stephen.patterson@noaa.gov>
2024-07-08 11:24:17 -0400

    Added GC check logic for project

--- inputs/sql/GetRelatedENC.sql
+++ inputs/sql/GetRelatedENC.sql
@@ -1,3 +1,4 @@
+-- 'GC11206.zip', 'US4SC22M', 'US4SC22M', 'AVAILABLE', '20', '2016\\GC''
 SELECT doc.documenttype, doc.BaseFileName, k.iecode, lk.status, doc.InformationCode, doc.Path
 FROM sourcedocument doc
     INNER JOIN affectedkapp aff ON aff.sourceid = doc.sourceid
