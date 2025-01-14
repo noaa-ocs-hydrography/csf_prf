eb5c56a5a7f7f5aa23eaec97cd49e65e8af62394
stephen.patterson <stephen.patterson@noaa.gov>
2025-01-14 14:34:56 -0500

    Filtered features for MHW setup

--- src/csf_prf/engines/MHWBufferEngine.py
+++ src/csf_prf/engines/MHWBufferEngine.py
@@ -1,6 +1,4 @@
-import os
-import requests
-import shutil
+import json
 import arcpy
 import pathlib
 
@@ -14,20 +12,58 @@ class MHWBufferEngine(Engine):
 
     def __init__(self, param_lookup: dict) -> None:
         self.param_lookup = param_lookup
+        self.features = {'COALNE': [], 'SLCONS': []}
+
+    def get_high_water_features(self):
+        """Read ENC features and build HW dataset"""
+
+        arcpy.AddMessage('Reading Feature records')
+        enc_files = self.param_lookup['enc_files'].valueAsText.replace("'", "").split(';')
+        for enc_path in enc_files:
+            # TODO should features be merged or unique across ENC files?
+            enc_file = self.open_file(enc_path)
+            enc_scale = pathlib.Path(enc_path).stem[2]
+            for layer in enc_file:
+                layer.ResetReading()
+                name = layer.GetDescription()
+                print(name)
+                if name == 'COALNE':
+                    print(name)
+                    self.store_coalne_features(layer)
+                elif name == 'SLCONS':
+                    print(name)
+                    self.store_slcons_features(layer)
+                elif name == 'LNDARE':
+                    print(name, layer)
+                    # TODO what is LNDARE for?
+                elif name == 'DSID':
+                    metadata = layer.GetFeature(0)
+                    metadata_json = json.loads(metadata.ExportToJson())
+                    resolution = metadata_json['properties']['DSPM_CSCL']
+                    scale_level = metadata_json['properties']['DSID_INTU']
+
+    def print_properties(self, feature) -> None:
+        for key, val in feature['properties'].items():
+            print(key, val)
+        print(feature['geometry'])
 
     def start(self) -> None:
         """Main method to begin process"""
 
+        self.set_driver()
         # TODO do we need to allow GC features input or download?
 
         # TODO use input ENC files or download ENC files?
 
-        # TODO get COALNE features with WATLEV blank or always dry
+        self.get_high_water_features()
+        # self.print_properties(self.features['SLCONS'][-1])
+        # self.print_properties(self.features['COALNE'][-1])
+        # open ENC files and read JSON features
+        # if table == MapInfo
+            # TODO get chart scale from MapInfo table
         # TODO get SLCONS features
             # TODO SLCONS 4, check if CONDTN not = piers/ruins
         
-        # TODO get chart scale from MapInfo table
-
         # TODO Intersect chart scale with all features
 
         # TODO add attribute values
@@ -42,3 +78,33 @@ class MHWBufferEngine(Engine):
 
         # TODO write out shapefile
         arcpy.AddMessage('Done')
+
+    def store_coalne_features(self, layer: list[dict]) -> None:
+        """Collect all COALNE features"""
+
+        for feature in layer:
+            if feature:
+                feature_json = json.loads(feature.ExportToJson())
+                props = feature_json['properties']
+                # TODO what is <Unfiltered> in FME?
+                if props['WATLEV'] in ['', '2']:  # TODO is empty string == FME <Blank>
+                    self.features['COALNE'].append(feature)
+
+    def store_slcons_features(self, layer: list[dict]) -> None:
+        """Collect all SLCONS features"""
+
+        for feature in layer:
+            if feature:
+                feature_json = json.loads(feature.ExportToJson())
+                props = feature_json['properties']
+                if props['CATSLC'] == '4':
+                    if props['WATLEV'] == '2':
+                        self.features['SLCONS'].append(feature)
+                    else:
+                        if props['CONDTN'] != '2':
+                            self.features['SLCONS'].append(feature)
+                else: # != 4
+                    # TODO what is <Unfiltered> in FME?
+                    if props['WATLEV'] in ['', '2']:  # TODO is empty string == FME <Blank>
+                        self.features['SLCONS'].append(feature)
+                    
---
+++ src/csf_prf/engines/run_mhwbufferengine.py
@@ -0,0 +1,28 @@
+import pathlib
+CSFPRF_MODULE = pathlib.Path(__file__).parents[2]
+
+import sys
+sys.path.append(str(CSFPRF_MODULE))
+from csf_prf.engines.MHWBufferEngine import MHWBufferEngine
+
+
+INPUTS = pathlib.Path(__file__).parents[3] / 'inputs'
+OUTPUTS = pathlib.Path(__file__).parents[3] / 'outputs'
+
+
+if __name__ == '__main__':
+    class Param:
+        def __init__(self, path):
+            self.path = path
+
+        @property
+        def valueAsText(self):
+            return self.path
+        
+    param_lookup = {
+        'sheets': Param(str(INPUTS / 'test_shapefiles' / 'enc_downloader_boundary.shp')),
+        'enc_files': Param(str(str(INPUTS / 'US4GA17M.000') + ';' + str(INPUTS / 'US5SC21M.000'))),
+        'output_folder': Param(str(OUTPUTS)),
+    }
+    engine = MHWBufferEngine(param_lookup)
+    engine.start()  
